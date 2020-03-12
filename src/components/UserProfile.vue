42043edd18e92fba77833bcfe3eb61de0deadf2d
Korawit Prutsachainimmit <korawit.p@gmail.com>
2020-03-12 13:23:56 +0700

    add remote repo

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <h2>Username : {{username}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            username : "korawit.p"
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Contact.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <h1>Contact</h1>
+      <h2>{{ address }}</h2>
+      <UserProfile></UserProfile>
+      <hello-world msg="Hello 1"></hello-world>
+      <hello-world msg="Hello 2"></hello-world>
+  </div>
+</template>
+
+<script>
+
+import HelloWorld from '@/components/HelloWorld.vue'
+import UserProfile from '@/components/UserProfile.vue'
+
+export default {
+    data() {
+        return {
+            address : "80 M1. PSU Phuket"
+        }
+    },
+    components: {
+        HelloWorld,
+        UserProfile
+    }
+}
+</script>
+
+<style>
+
+</style>
